fdc747602cb563eac7fed65f352da477d9362f23
Matt McCormick <matt.mccormick@kitware.com>
2014-04-13 22:14:55 -0400

    Write graphs to JSON files.

--- src/create_graphs.py
+++ src/create_graphs.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+import json
 import matplotlib.pyplot as plt
 import networkx as nx
+from networkx.readwrite import json_graph
 import numpy as np
 
 branching_factor = 4
@@ -14,6 +16,9 @@ graph = nx.star_graph(number_of_nodes)
 pos = nx.spring_layout(graph)
 pos = nx.graphviz_layout(graph, prog='twopi', args='')
 nx.draw(graph, pos, with_labels=False)
+data = json_graph.node_link_data(graph)
+with open('figures/bdfl_graph.json', 'wb') as fp:
+    json.dump(data, fp)
 
 ax = fig.add_subplot(3, 1, 2)
 graph = nx.balanced_tree(branching_factor, tree_height)
@@ -21,6 +26,8 @@ tree_edges = len(graph.edges())
 pos = nx.graphviz_layout(graph, prog='twopi', args='')
 tree_degrees = nx.degree(graph)
 nx.draw(graph, pos, with_labels=False)
+with open('figures/hierachy_graph.json', 'wb') as fp:
+    json.dump(data, fp)
 
 ax = fig.add_subplot(3, 1, 3)
 degrees = tree_degrees.values()
@@ -30,5 +37,7 @@ graph = nx.barabasi_albert_graph(number_of_nodes, 2,
 graph = graph.to_undirected()
 pos = nx.spring_layout(graph, k=0.12, iterations=500)
 nx.draw(graph, pos, with_labels=False)
+with open('figures/community_graph.json', 'wb') as fp:
+    json.dump(data, fp)
 
 plt.show()
