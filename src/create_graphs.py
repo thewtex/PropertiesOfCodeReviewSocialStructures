bc88f8ed7d1b60b6f9d777d3757dea248e8999e8
Matt McCormick <matt.mccormick@kitware.com>
2014-04-21 15:18:39 -0400

    Compute betweenness, closeness.

--- src/create_graphs.py
+++ src/create_graphs.py
@@ -13,6 +13,12 @@ number_of_nodes = branching_factor ** tree_height
 fig = plt.figure(1)
 ax = fig.add_subplot(3, 1, 1)
 graph = nx.star_graph(number_of_nodes)
+betweenness = nx.centrality.betweenness_centrality(graph)
+for node, between in betweenness.iteritems():
+    graph.node[node]['betweenness'] = between
+closeness = nx.centrality.closeness_centrality(graph)
+for node, close in closeness.iteritems():
+    graph.node[node]['closeness'] = close
 pos = nx.spring_layout(graph)
 pos = nx.graphviz_layout(graph, prog='twopi', args='')
 nx.draw(graph, pos, with_labels=False)
@@ -22,6 +28,12 @@ with open('figures/bdfl_graph.json', 'wb') as fp:
 
 ax = fig.add_subplot(3, 1, 2)
 graph = nx.balanced_tree(branching_factor, tree_height)
+betweenness = nx.centrality.betweenness_centrality(graph)
+for node, between in betweenness.iteritems():
+    graph.node[node]['betweenness'] = between
+closeness = nx.centrality.closeness_centrality(graph)
+for node, close in closeness.iteritems():
+    graph.node[node]['closeness'] = close
 tree_edges = len(graph.edges())
 pos = nx.graphviz_layout(graph, prog='twopi', args='')
 tree_degrees = nx.degree(graph)
@@ -36,6 +48,12 @@ np.random.shuffle(degrees)
 graph = nx.barabasi_albert_graph(number_of_nodes, 2,
                                  seed=222)
 graph = graph.to_undirected()
+betweenness = nx.centrality.betweenness_centrality(graph)
+for node, between in betweenness.iteritems():
+    graph.node[node]['betweenness'] = between
+closeness = nx.centrality.closeness_centrality(graph)
+for node, close in closeness.iteritems():
+    graph.node[node]['closeness'] = close
 pos = nx.spring_layout(graph, k=0.12, iterations=500)
 nx.draw(graph, pos, with_labels=False)
 data = json_graph.node_link_data(graph)
